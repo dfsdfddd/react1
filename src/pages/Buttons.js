8e463d9e49d34524c09aaaaa61e86030fe933b41
刘武 <1004928208@qq.com>
2020-03-23 09:16:32 +0800

    添加col和row

--- src/pages/Buttons.js
+++ src/pages/Buttons.js
@@ -1,7 +1,68 @@
 import React,{Component,Fragment} from 'react';
-  import {Table} from 'antd';
+  import { Table,Form, Input, Button, Checkbox,Row, Col,Card } from 'antd';
 
 class Buttons extends Component{
+  _searchBar(){
+    const layout = {
+      labelCol: { span: 8 },
+      wrapperCol: { span: 16 },
+    };
+    const tailLayout = {
+      wrapperCol: { offset: 8, span: 16 },
+    };
+
+    const onFinish = values => {
+      console.log('Success:', values);
+    };
+  
+    const onFinishFailed = errorInfo => {
+      console.log('Failed:', errorInfo);
+    };
+    
+    return (
+      <Form
+      {...layout}
+      layout={'vertical'}
+      name="basic"
+      initialValues={{ remember: true }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+    >
+       <Row>
+        <Col span={6}>
+          <Form.Item
+          label="Username"
+          name="username"
+          rules={[{ required: true, message: 'Please input your username!' }]}
+        >
+          <Input />
+        </Form.Item>
+        </Col>
+        <Col span={6}>
+        <Form.Item
+            label="Password"
+            name="password"
+            rules={[{ required: true, message: 'Please input your password!' }]}
+          >
+            <Input.Password />
+          </Form.Item>
+        </Col>
+        <Col span={6}>
+        <Form.Item style={{marginTop:40}} {...tailLayout} name="remember" valuePropName="checked">
+          <Checkbox>Remember me</Checkbox>
+        </Form.Item>
+        </Col>
+        <Col span={6}>
+        <Form.Item {...tailLayout} style={{marginTop:40}}>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+        </Col>
+      </Row>
+    </Form>
+    )
+  }
   render(){
     const dataSource = [
       {
@@ -37,8 +98,12 @@ class Buttons extends Component{
     ];
     return (
       <Fragment>
-        <h1>Buttons</h1>
+        <Card title="查询条件" style={{marginBottom:20}}>
+        {this._searchBar()}
+       </Card>
+       <Card title="列表" >
         <Table bordered={true} dataSource={dataSource} columns={columns} />
+       </Card>
       </Fragment>
     
     )
